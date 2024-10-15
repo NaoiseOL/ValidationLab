c4db197ab12a8f713b2ee8f2f6a325ad0a95f6ea
NaoiseOL <G00385498@atu.ie>
2024-10-15 10:01:10 +0100

    Created Project and added Person class

--- src/main/java/ie/atu/week4lab/Person.java
+++ src/main/java/ie/atu/week4lab/Person.java
@@ -1,11 +1,36 @@
 package ie.atu.week4lab;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class Person {
+
+    @NotBlank(message = "Name cannot be Blank")
+    private String name;
+
+    @NotBlank(message = "Title cannot be Blank")
+    private String title;
+
+    @NotBlank(message = "Employment status cannot be Blank")
+    private String employed;
+
+    @NotNull(message = "Age cannot be Null")
+    private int age;
+
+    @Email(message = "Must be a valid email")
+    private String email;
+
+    @NotBlank(message = "Position cannot be Blank")
+    private String position;
+
+    @NotBlank(message = "Department cannot be Blank")
+    private String department;
+
 
 
 }
