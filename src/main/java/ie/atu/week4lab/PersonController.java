a53c2896e36489f844f258384ade839e54641ea9
NaoiseOL <G00385498@atu.ie>
2024-10-15 11:01:37 +0100

    Added Get function in PersonController

--- src/main/java/ie/atu/week4lab/PersonController.java
+++ src/main/java/ie/atu/week4lab/PersonController.java
@@ -1,14 +1,24 @@
 package ie.atu.week4lab;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
-    @PostMapping("/person/createPerson")
+    private List<Person> personList = new ArrayList<>();
+
+    @GetMapping
+    public List<Person> getPersonList(){
+        return personList;
+    }
+
+    @PostMapping("/createPerson")
     public String createPerson(@RequestBody @Valid Person createPerson){
+        personList.add(createPerson);
         return "Person Created";
     }
 }
